b565a50a63a5eba354736ab586cbd4818eb08e66
David Overton <david@overtons.id.au>
2015-10-28 05:12:43 +0100

    Fixes to Queens2 and SendMoreMoney.

--- src/Queens2.hs
+++ src/Queens2.hs
@@ -1,20 +1,20 @@
-module Queens (nQueens) where
+module Queens2 (nQueens) where
 
 import FD
 
-nQueens :: Int -> FD s [FDVar s]
+nQueens :: Int -> FD [FDVar]
 nQueens n = do
     qs <- newVars n (1, n)
     safeQueens qs
     return qs
 
-safeQueens :: [FDVar s] -> FDConstraint s
+safeQueens :: [FDVar] -> FDConstraint
 safeQueens [] = return ()
 safeQueens (q : qs) = do
     safeQueen qs q 1
     safeQueens qs
 
-safeQueen :: [FDVar s] -> FDVar s -> Int -> FDConstraint s
+safeQueen :: [FDVar] -> FDVar -> Int -> FDConstraint
 safeQueen [] _ _ = return ()
 safeQueen (q : qs) q0 d = do
    q0 #\= q 
